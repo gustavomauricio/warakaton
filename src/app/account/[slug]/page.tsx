5f2abec197e71ee417c6d0a0a84f75faaf999de9
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2023-11-03 19:15:42 +0000

    account page avatar

--- src/app/account/[slug]/page.tsx
+++ src/app/account/[slug]/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import React from "react";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 function Account({ params }: { params: { slug: string } }) {
   return (
     <div>
       <h1 className="text-3xl font-extrabold text-center mb-4">User Account</h1>
-      <p>{params.slug}</p>
+      <div className="mb-10">
+        <p>{params.slug}</p>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
       <Button onClick={() => alert("TODO")}>Send Gift</Button>
     </div>
   );
