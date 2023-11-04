a78a49a46597d7ac5546001298a429f356fc3496
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2023-11-04 15:50:11 +0000

    config contract addresses

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+const contracts = {
+  soulboundBadges: "0x8b0242F40b0428715475254d37853Ea13b83b610",
+  usersDB: "0x4D76E5Cc49091563BB3203657E11c485daAB4681",
+  communityVault: "0x48f27B0094d907417F781294bCd9fc48C4F556fB",
+  nftGifts: "0xCF85C0DB8b670c3AADa28B47b735B085BFCF2c9d",
+};
+
+export { contracts };
