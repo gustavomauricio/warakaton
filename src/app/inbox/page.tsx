dbdb7a853565927b16d23b69bf20164bbf0f62ad
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2023-11-04 22:24:23 +0000

    padding

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -3,7 +3,7 @@ import InboxExample from "@/components/inbox-example";
 
 function Inbox() {
   return (
-    <div>
+    <div className="max-w-full py-8">
       <h1 className="text-3xl font-extrabold text-center mb-4">Inbox</h1>
       <InboxExample />
     </div>
