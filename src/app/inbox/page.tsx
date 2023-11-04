8da786b26bd59d156e073076ce9881b29b783250
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2023-11-04 22:28:54 +0000

    styling

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -3,7 +3,7 @@ import InboxExample from "@/components/inbox-example";
 
 function Inbox() {
   return (
-    <div className="max-w-full py-8">
+    <div className="max-w-full py-8 max-w-4xl">
       <h1 className="text-3xl font-extrabold text-center mb-4">Inbox</h1>
       <InboxExample />
     </div>
