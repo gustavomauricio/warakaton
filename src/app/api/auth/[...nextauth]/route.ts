08fd147a08978ed8e4eb83186fc643a581dccf13
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2023-11-04 22:47:02 +0000

    prettier

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,40 +1,42 @@
-import NextAuth from "next-auth"
-import type { AuthOptions } from "next-auth"
-import TwitterProvider from "next-auth/providers/twitter"
+import NextAuth from "next-auth";
+import type { AuthOptions } from "next-auth";
+import TwitterProvider from "next-auth/providers/twitter";
 
 export const authOptions: AuthOptions = {
   providers: [
     TwitterProvider({
       clientId: process.env.TWITTER_ID as string,
       clientSecret: process.env.TWITTER_SECRET as string,
-    })
+    }),
   ],
   callbacks: {
     async signIn({ profile }) {
       if (profile) {
         const { screen_name, description } = profile as {
-          screen_name: string
-          description: string
+          screen_name: string;
+          description: string;
         };
 
-        if (description.length === 42 && description.startsWith('0x')) {
-          const apiUrl = `${process.env.API_URL}/submit_twitter_handle?address=${encodeURIComponent(
+        if (description.length === 42 && description.startsWith("0x")) {
+          const apiUrl = `${
+            process.env.API_URL
+          }/submit_twitter_handle?address=${encodeURIComponent(
             description
-        )}&twitter_token=${encodeURIComponent(screen_name)}`;
-        try {
-          console.log(apiUrl)
-          const response = await fetch(apiUrl, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          });
+          )}&twitter_token=${encodeURIComponent(screen_name)}`;
+          try {
+            console.log(apiUrl);
+            const response = await fetch(apiUrl, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
 
-          const data = await response.json();
-          console.log("Response data:", data);
-        } catch (error) {
-          console.error("Error submitting Twitter handle:", error);
-        }
+            const data = await response.json();
+            console.log("Response data:", data);
+          } catch (error) {
+            console.error("Error submitting Twitter handle:", error);
+          }
         }
       }
 
@@ -44,6 +46,6 @@ export const authOptions: AuthOptions = {
   session: { strategy: "jwt" },
 };
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
