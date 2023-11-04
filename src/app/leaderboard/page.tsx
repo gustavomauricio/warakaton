035856639ea25f2acabf5c0a556f8771dd589eb1
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2023-11-04 17:22:25 +0000

    leaderboard page improvements

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -15,58 +15,132 @@ const fetchLeaderBoardData = async () => {
   // const data = await res.json();
   // return data;
 
-  return [
-    {
-      address: "0x06Ad5f18F8A772f07429A70Fb877da7937008694",
-      twitterHandle: "@musk",
-      amountUsd: 6666,
-    },
-    {
-      address: "0xD3CFE0E43c86BD38fBadB023e4Fe3bc1aefec902",
-      twitterHandle: "@corno",
-      amountUsd: 123,
-    },
-    {
-      address: "0xD3CFE0E43c86BD38fBadB023e4Fe3bc1aefec902",
-      twitterHandle: "@whatever",
-      amountUsd: 456,
-    },
-  ];
+  return {
+    topDonators: [
+      {
+        address: "0x06Ad5f18F8A772f07429A70Fb877da7937008694",
+        twitterHandle: "@musk",
+        amountUsd: 6666,
+      },
+      {
+        address: "0xD3CFE0E43c86BD38fBadB023e4Fe3bc1aefec902",
+        twitterHandle: "@corno",
+        amountUsd: 123,
+      },
+      {
+        address: "0xD3CFE0E43c86BD38fBadB023e4Fe3bc1aefec902",
+        twitterHandle: "@whatever",
+        amountUsd: 456,
+      },
+      {
+        address: "0xD3CFE0E43c86BD38fBadB023e4Fe3bc1aefec902",
+        twitterHandle: "@whatever",
+        amountUsd: 456,
+      },
+      {
+        address: "0xD3CFE0E43c86BD38fBadB023e4Fe3bc1aefec902",
+        twitterHandle: "@whatever",
+        amountUsd: 456,
+      },
+    ],
+    topReceivers: [
+      {
+        address: "0x06Ad5f18F8A772f07429A70Fb877da7937008694",
+        twitterHandle: "@musk",
+        amountUsd: 6666,
+      },
+      {
+        address: "0xD3CFE0E43c86BD38fBadB023e4Fe3bc1aefec902",
+        twitterHandle: "@corno",
+        amountUsd: 123,
+      },
+      {
+        address: "0xD3CFE0E43c86BD38fBadB023e4Fe3bc1aefec902",
+        twitterHandle: "@whatever",
+        amountUsd: 456,
+      },
+      {
+        address: "0xD3CFE0E43c86BD38fBadB023e4Fe3bc1aefec902",
+        twitterHandle: "@whatever",
+        amountUsd: 456,
+      },
+      {
+        address: "0xD3CFE0E43c86BD38fBadB023e4Fe3bc1aefec902",
+        twitterHandle: "@whatever",
+        amountUsd: 456,
+      },
+    ],
+  };
 };
 
 async function Leaderboard() {
   const leaderboardData = await fetchLeaderBoardData();
 
   return (
-    <div className="max-w-full">
+    <div className="max-w-full py-8">
       <h1 className="text-3xl font-extrabold text-center mb-8">Leaderboard</h1>
 
-      <Table>
-        {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Rank</TableHead>
-            <TableHead>Address</TableHead>
-            <TableHead>Twitter</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {leaderboardData.map((row, index) => (
-            <TableRow key={row.address}>
-              <TableCell className="font-medium">{index + 1}</TableCell>
-              <TableCell className="font-medium">{row.address}</TableCell>
-              <TableCell>{row.twitterHandle}</TableCell>
-              <TableCell>
-                {new Intl.NumberFormat("en-US", {
-                  style: "currency",
-                  currency: "USD",
-                }).format(row.amountUsd)}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <div className="flex flex-col gap-y-10">
+        <div>
+          <p className="mb-4 font-medium">Top Donators:</p>
+          <Table>
+            {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Rank</TableHead>
+                <TableHead>Address</TableHead>
+                <TableHead>Twitter</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {leaderboardData.topDonators.map((row, index) => (
+                <TableRow key={row.address}>
+                  <TableCell className="font-medium">{index + 1}</TableCell>
+                  <TableCell className="font-medium">{row.address}</TableCell>
+                  <TableCell>{row.twitterHandle}</TableCell>
+                  <TableCell>
+                    {new Intl.NumberFormat("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    }).format(row.amountUsd)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+
+        <div>
+          <p className="mb-4 font-medium">Top Donators:</p>
+          <Table>
+            {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Rank</TableHead>
+                <TableHead>Address</TableHead>
+                <TableHead>Twitter</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {leaderboardData.topReceivers.map((row, index) => (
+                <TableRow key={row.address}>
+                  <TableCell className="font-medium">{index + 1}</TableCell>
+                  <TableCell className="font-medium">{row.address}</TableCell>
+                  <TableCell>{row.twitterHandle}</TableCell>
+                  <TableCell>
+                    {new Intl.NumberFormat("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    }).format(row.amountUsd)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
     </div>
   );
 }
