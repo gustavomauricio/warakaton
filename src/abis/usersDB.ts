e1cfb4947cd477084e36e527e868d5744565ed36
b8511 <b-c-gomes@hotmail.com>
2023-11-04 16:10:59 +0000

    add configs

---
+++ src/abis/usersDB.ts
@@ -0,0 +1,167 @@
+const usersDBAbi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor"
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address"
+      }
+    ],
+    name: "OwnableInvalidOwner",
+    type: "error"
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address"
+      }
+    ],
+    name: "OwnableUnauthorizedAccount",
+    type: "error"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address"
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address"
+      }
+    ],
+    name: "OwnershipTransferred",
+    type: "event"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "userAddress",
+        type: "address"
+      },
+      {
+        indexed: true,
+        internalType: "string",
+        name: "userHandle",
+        type: "string"
+      }
+    ],
+    name: "userAdded",
+    type: "event"
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "handle_",
+        type: "string"
+      }
+    ],
+    name: "getAddressFromTwitterHandle",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "address_",
+        type: "address"
+      }
+    ],
+    name: "getTwitterHandleFromAddress",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address"
+      }
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "address_",
+        type: "address"
+      },
+      {
+        internalType: "string",
+        name: "twitterHandle_",
+        type: "string"
+      }
+    ],
+    name: "writeTwitterHandle",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool"
+      }
+    ],
+    stateMutability: "nonpayable",
+    type: "function"
+  }
+]
+  
+  export { usersDBAbi };
+  
+
+  //TODO: change this when new contracts are deployed
