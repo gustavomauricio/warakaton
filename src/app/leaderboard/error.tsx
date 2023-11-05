47d9f877f0c8aedd381f0899c13ccdbe623f5d44
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2023-11-05 11:37:48 +0000

    error pages

--- src/app/leaderboard/error.tsx
+++ src/app/leaderboard/error.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+"use client"; // Error components must be Client Components
 
-const error = () => {
-  return <div>Error fetching user information!</div>;
-};
+import { useEffect } from "react";
 
-export default error;
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
